2231d915531408434935b7db39055773a8fadc4f
KateM3d <ekaterina8medvedeva@gmail.com>
2021-07-27 13:31:41 -0400

    added all events

---
+++ script.js
@@ -0,0 +1,43 @@
+const button = document.querySelector('#btn');
+
+
+button.addEventListener('click', calculateAmount)
+
+const buttonTip = document.querySelector('#addTip');
+buttonTip.addEventListener('click', showTip);
+
+
+function showTip(e) {
+    e.preventDefault();
+    tip.style.display = "block";
+}
+
+function calculateAmount(e) {
+    e.preventDefault();
+    const bill = document.querySelector('#bill').value;
+    const people = document.querySelector('#people').value;
+    const tip = document.querySelector('#tip').value;
+
+    if (bill.trim() === "" || people.trim() === "" || people < 1) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            text: 'Please enter your information!',
+
+        })
+    }
+
+    let amountPerPerson = bill / people;
+
+    let tipPerPerson = (bill * tip) / people;
+
+    let totalSum = amountPerPerson + tipPerPerson;
+
+    console.log(totalSum);
+
+
+    document.querySelector('#dividedBill').textContent = amountPerPerson.toFixed(2);
+    document.querySelector('#dividedTip').textContent = tipPerPerson.toFixed(2);
+    document.querySelector('#billAndTip').textContent = totalSum.toFixed(2);
+
+}
